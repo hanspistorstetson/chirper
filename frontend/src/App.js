9d8e79d14b253dc142a23c88dbfefdcd359e5fb8
Hans Pistor <hpistor@ad.stetson.edu>
2019-04-01 18:16:38 -0400

    react app init + menu bar

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Container} from "semantic-ui-react";
+import Login from "./components/Login";
+import NavBar from './components/NavBar'
+
+const App = () => (
+    <Container>
+        <NavBar />
+    </Container>
+);
+
+export default App;
